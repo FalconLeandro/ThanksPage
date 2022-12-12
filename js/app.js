4c2435368ce7e7d0b366022a1fec797fa1e7e8bf
Leandro Falcon <cronosteo1034@gmail.com>
2022-12-12 01:48:21 -0300

    arreglando ruta script

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 window.addEventListener('load',()=>{
     setTimeout(() => {
+        console.log('se cumplieron 5 segundos');
         window.location.href = 'https://laberintodeborges.com/#contacto';
     }, 5000);
 })
